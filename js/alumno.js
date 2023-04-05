9031f2e549bd8521917f5866b519b89eb1dccb33
dec-h1 <doracallisaya21@gmail.com>
2023-04-04 22:38:34 -0500

    corriguiendo 2

--- js/alumno.js
+++ js/alumno.js
@@ -6,11 +6,7 @@ const alumnos=
         apellido: "Mamani Hinojosa",
         edad: 18,
         celular: "932445676",
-        descripcion: "ella es la mas antigua, grado 2do kyu, 2do marron",
-        mensual: 150,
-        grado: "2do kyu",
-        universitario: 1,
-        cantidad:0
+        mensual: 150, 
     },
 
     {
@@ -19,60 +15,6 @@ const alumnos=
         apellido: "mendoza perez",
         edad: 38,
         celular: "932434343",
-        descripcion: "ella es la mas antigua, grado 6to kyu,1er verde",
         mensual: 150,
-        grado: "6to kyu",
-        universitario: 0,
-        cantidad:0
-    },
-
-    {
-        id: 3,
-        nombre: "bryam",
-        apellido: "Mamani Hinojosa",
-        edad: 20,
-        celular: "932445676",
-        descripcion: "ella es la mas antiguo, grado 1Dan, 1er cinturon negro",
-        mensual: 150,
-        grado: "1er Dan",
-        universitario: 1,
-        cantidad:0
-    },
-
-    {
-        id: 4,
-        nombre: "fabricio",
-        apellido: "ramos Hinojosa",
-        edad: 18,
-        celular: "932445676",
-        descripcion: "ella es la mas antiguo, grado 7mo kyu",
-        mensual: 150,
-        grado: "7mo kyu",
-        cantidad:0
-    },
-
-    {
-        id: 5,
-        nombre: "javier",
-        apellido: "Barreto",
-        edad: 13,
-        celular: "932445676",
-        descripcion: "ella es la mas antiguo, grado 6to kyu",
-        mensual: 150,
-        grado: "6to kyu",
-        cantidad:0
-    },
-
-    {
-        id: 6,
-        nombre: "zulma",
-        apellido: "Calderon",
-        edad: 35,
-        celular: "932445676",
-        descripcion: "ella es la mas antigua, grado 6to kyu",
-        mensual: 150,
-        grado: "6to kyu",
-        universitario: 0,
-        cantidad:0
     }
 ]
--- js/registroalum.js
+++ js/registroalum.js
@@ -5,6 +5,17 @@ let cen=0
 let tope=0
 let max=0
 let nombre=[];
+let apellido=[];
+let celular=[];
+let edad=[];
+let mensual=[];
+let year=[];
+/* const alumnos=[];
+ojo ahi 
+let miId=0*/
+let miId=alumnos.length;
+
+let precio=150;
 seleccionarMatricula=seleccionarMatricula;
 if(seleccionarMatricula==""){
     alert("Por favor ingresa el numero de opcion de la matricula")
@@ -41,21 +52,21 @@ else{
 }
   
 function matricula(){
-    
+ 
 let seleccionarMatricula=Number(prompt( "1-Si eres Universitario 20% por descuento de la mensualidad \r 2-No eres universitario paga la matricula 20 y la mensualidad 150 "))
-let total=0;
-let precio=150;
+let total=0;  
  while (seleccionarMatricula != 0 ) {
     switch (seleccionarMatricula) {
       case 1:
         total=total+1
-        llenarDatos() 
-        
+        llenarDatos(seleccionarMatricula) 
+        tope++;
         break;
       case 2:
         total=total+1
         mensual= precio
-        llenarDatos() 
+        llenarDatos(seleccionarMatricula) 
+        tope++;
         break;
   
       default:
@@ -63,7 +74,9 @@ let precio=150;
     }
     seleccionarMatricula=Number(prompt( "1-Si eres Universitario 20% por descuento de la mensualidad \r 2-No eres universitario paga la matricula 20 y la mensualidad 150 \r 3- Salir 0"))
   }
-
+/* 
+     cont += nombre[tope] + "\r "+apellido[tope]+"\r ";
+    alert(cont);  */
   alert("el total de la persona esta inscrita: " + total +" personas")
 
 //inserta datos
@@ -77,67 +90,93 @@ let precio=150;
     
 }
 function mostrarMatriculados(){
- 
-  for(let i=1;i<=tope;i++){
-        cont1 += nombre[i] + "\r";
+ let texto="";
+  for(const alumno of alumnos){
+        texto += `
+        id: ${alumno.id},
+        nombre: ${alumno.nombre},
+        apellido: ${alumno.apellido},
+        edad: ${alumno.edad},
+        celular: ${alumno.celular},
+        mensual:${alumno.mensual},
+        \r`
     } 
-    alert(cont1);
+    alert(texto);
 }
 function borrarAlumno(){
    let x;
-   if(tope>0){
+   if(alumnos.length>0){
     alert("El registro esta conformado de la siguiente manera");
     mostrarMatriculados();
-    if(opcion==2)
-        x=Number(prompt("Escoge el numero de elemento que desea eliminar?"))
-    else
-        x=Number(prompt("Escoge el numero de elemento que desea modificar"))
-    let i=1;
-    while (i<=tope)
-    {
-        if( i==x)
-        {
-            for(let j=i;j<=tope;j++){
-                nombre[j]=nombre[j+1];
-            }
-            tope=tope-1;
-            i=tope+1;
-            cen=1;
-        }
-        else{
-            i++;
-            cen=0;
-        }
+    x=Number(prompt("Escoge el Id que desea eliminar?"))
+    const indice = alumnos.findIndex((alumno)=>alumno.id==x) 
+    if(indice==-1){
+        alert("No existe el registro")
+
+    }else {
+        alumnos.splice(indice,1)
+        alert("se elimino el registro") 
     }
-    if(cen==0){
-        alert("No existe el registro,"+ x)
-        }
-    mostrarMatriculados();
-    }
+   }
     else
         alert("El registro esta vacio")
 }
 function editarAlumno(){
-   borrarAlumno();
-   if(cen==1)
-        matricula();
+    let x;
+    if(alumnos.length>0){
+     alert("El registro esta conformado de la siguiente manera");
+     mostrarMatriculados();
+     x=Number(prompt("Escoge el Id que desea modificar?"))
+     const indice = alumnos.findIndex((alumno)=>alumno.id==x) 
+     if(indice==-1){
+         alert("No existe el registro")
+     }else {
+        let nombre = prompt('Cual es tu nombre?'," ")
+        let apellido= prompt('Cual es tu apellido?'," ")
+        let edad = prompt('Cual es tu edad?'," ")
+        let celular=Number(prompt('Cual es tu numero de celular?'," "))
+         alumnos[indice].nombre=nombre
+         alumnos[indice].apellido=apellido
+         alumnos[indice].edad=edad
+         alumnos[indice].celular=celular
+         alert("se actualizo el registro")  
+     }
+    }
+     else
+         alert("El registro esta vacio")
 }
-
-
 //datos
-function llenarDatos(){
+function llenarDatos(seleccionarMatricula){
 
+    const datos=[]
     let nombre = prompt('Cual es tu nombre?'," ")
+    nombre[tope+1] = nombre;
     let apellido= prompt('Cual es tu apellido?'," ")
+    apellido[tope+1] = apellido;
     let edad = prompt('Cual es tu edad?'," ")
+    edad[tope+1] = edad;
     let celular=Number(prompt('Cual es tu numero de celular?'," "))
+    celular[tope+1] = celular;
     const currentDate = new Date()
     const year = currentDate.getFullYear() - Number(edad)
+    year[tope+1] = year;
+    let mensualTotal
     if(seleccionarMatricula==1){
-      mensual= precio-precio*0.2
-    }else {mensual= precio}
-
-    const datos = "\r Nombre: "+ nombre+ "\r apellido: " + apellido+"\r edad: "+edad+"\r Nro de Celular: "+celular+"\r Año: "+year+"\r mensual: "+mensual;
-    alert("lista registrada: " + datos)
- 
+        mensualTotal=precio-precio*0.2
+      mensual[tope+1]= precio-precio*0.2
+    }else {mensual[tope+1]= precio
+        mensualTotal=precio}
+    console.log(nombre,apellido,edad,celular)
+    miId++
+    alumnos.push({
+            id: miId,
+            nombre: nombre,
+            apellido: apellido,
+            edad: edad,
+            celular: celular,
+            mensual:mensualTotal,
+    })
+    datos[tope+1] = "\r Nombre: "+ nombre[tope+1]+ "\r apellido: " + apellido[tope+1]+"\r edad: "+edad[tope+1]+"\r Nro de Celular: "+celular[tope+1]+"\r Año: "+year[tope+1]+"\r mensual: "+mensual[tope+1];
+   /*  alert("lista registrada: " + datos[tope+1])
+  */
  }
